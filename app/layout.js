86c752fc2d52a7f2febcae1c74fb3e444406ab1f
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-17 13:22:58 -0300

    intial layout

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+// entry point of app - wrap the app in a layout
+import React from "react";
+import "../styles/globals.css";
+
+import Nav from "@/components/Nav";
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <main>
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
