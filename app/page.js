831163aa3ff8159bba94dd7cc07d0a9a8e94c273
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-18 02:23:30 -0300

    home +adjust

--- app/page.js
+++ app/page.js
@@ -1,8 +1,25 @@
 // homepage of the app
 import React from "react";
+import Link from "next/link";
+
+import { Card } from "../components/ui/card";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-screen flex justify-center items-center">
+      <h3 className="text-5xl font-extrabold text-slate-600">
+        Select an option and get started!
+      </h3>
+      <div className="flex justify-center items-center">
+        <Link href="/upload">
+          <Card />
+        </Link>
+        <Link href="/search">
+          <Card />
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
