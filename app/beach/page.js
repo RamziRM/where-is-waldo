75227977d150718a477adae84485712e17ff5039
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-18 14:40:09 -0300

    beach page

---
+++ app/beach/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const Beach = () => {
+  return (
+    <div>
+      <Image src="/assets/images/wallwaldo2.jpg" width={10000} height={10000} />
+    </div>
+  );
+};
+
+export default Beach;
