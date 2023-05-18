63ed49486eae6aee25d968d71391dc06517d9c29
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-18 14:40:17 -0300

    space page

---
+++ app/space/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const Space = () => {
+  return (
+    <div className="bg-slate-600 h-full w-full">
+      <Image src="/assets/images/wallwaldo1.jpg" width={10000} height={10000} />
+    </div>
+  );
+};
+
+export default Space;
