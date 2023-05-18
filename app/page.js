43259bb4e3e169074009183e4fe1ed77a2d68982
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-18 14:20:19 -0300

    homepage Card comp x2 - done

--- app/page.js
+++ app/page.js
@@ -1,21 +1,61 @@
 // homepage of the app
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-import { Card } from "../components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
 
 const Home = () => {
   return (
-    <section className="w-full h-screen flex justify-center items-center">
-      <h3 className="text-5xl font-extrabold text-slate-600">
-        Select an option and get started!
+    <section className="w-full h-screen flex flex-col items-center gap-5">
+      <h3 className="text-3xl font-extrabold text-slate-600 mt-4">
+        Select an option to play
       </h3>
-      <div className="flex justify-center items-center">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10">
         <Link href="/upload">
-          <Card />
+          <Card>
+            <CardHeader>
+              <CardTitle>The beach</CardTitle>
+              <CardDescription>
+                Waldo and company, at a crowded beach
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src="/assets/images/wallwaldo2.jpg"
+                width={300}
+                height={300}
+              />
+            </CardContent>
+            <CardFooter>
+              <p>Click to get started!</p>
+            </CardFooter>
+          </Card>
         </Link>
         <Link href="/search">
-          <Card />
+          <Card>
+            <CardHeader>
+              <CardTitle>Interplanetary civilization</CardTitle>
+              <CardDescription>Waldo and company, on the moon?</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src="/assets/images/wallwaldo1.jpg"
+                width={300}
+                height={300}
+              />
+            </CardContent>
+            <CardFooter>
+              <p>Click to get started!</p>
+            </CardFooter>
+          </Card>
         </Link>
       </div>
     </section>
